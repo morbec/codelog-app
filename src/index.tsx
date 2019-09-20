64c6061d9dda8daf9fc96af3459393c32e6aa138
Fabio Morbec <morbecfm@gmail.com>
2019-09-20 22:59:44 +0100

    Seed the 'database' (temporarly)

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,41 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import { addNewCodelog, deleteAll } from './api'
 import App from './App'
 import * as serviceWorker from './serviceWorker'
+import Codelog from './types/codelog'
+
+// TODO: Delete this!
+const seed = () => {
+  const codelogs: Codelog[] = [
+    {
+      id: 666,
+      date: new Date(),
+      title: 'foo',
+      tasks: 'make foo bar'
+    },
+    {
+      id: 555,
+      date: new Date(),
+      title: 'foobar',
+      tasks: 'unmake foo bar'
+    },
+    {
+      id: 444,
+      date: new Date(),
+      title: 'title cames here',
+      tasks: 'do or do not'
+    }
+  ]
+
+  deleteAll()
+  codelogs.forEach((codelog) => {
+    addNewCodelog(codelog)
+  })
+  console.log(`length ==> ${codelogs.length}`)
+}
+
+seed()
 
 ReactDOM.render(<App />, document.getElementById('root'))
 
