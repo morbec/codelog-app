646b5eb44891e4275daecc5e07d260a4f858e8c3
Fabio Morbec <morbecfm@gmail.com>
2019-09-23 14:47:18 +0100

    Refactor some functions of the API and update tests

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,92 +1,73 @@
-import Codelog from '../types/codelog'
+/* eslint-disable no-unused-vars */
+import { Codelog } from '../store/codelog/types'
 
-/**
- * Clear the localStorage
- */
-export const deleteAll = () => {
-  localStorage.clear()
-}
-
-/**
- * Save the codelogs array in the localStorage
- * @param codelogs - Array containing the codelogs
- */
+// private
 const saveCodelog = (codelogs: Codelog[]) => {
   // deleteAll()
   localStorage.setItem('codelogs', JSON.stringify(codelogs))
 }
 
-/**
- * Get the list (array) of codelogs stored in the localStorage
- * @returns [Codelog] | []
- */
-export const getAllCodelogs = (): [Codelog] | [] => {
+const generateId = (): number => {
+  return parseInt(
+    Math.random()
+      .toString()
+      .split('.')[1]
+  )
+}
+
+// public
+
+export const deleteAll = () => {
+  localStorage.clear()
+}
+
+export const getAllCodelogs = (): Codelog[] | [] => {
   const localData = localStorage.getItem('codelogs')
   return localData ? JSON.parse(localData) : []
 }
 
-/**
- * Get a codelog by its id
- * @param id Codelog's id
- * @returns Codelog | Error - If a codelog is found or Error in case the database is empty or the id does not exist
- */
-export const getCodelogById = (id: number): Codelog | Error => {
+export const getCodelogById = (id: number): Codelog => {
   const codelogs = getAllCodelogs()
 
-  if (!codelogs) throw Error('Database is empty')
+  if (!codelogs) throw new Error('Database is empty')
 
   const codelogIndex = codelogs.findIndex((codelog) => codelog.id === id)
   if (codelogIndex === -1) {
-    return Error('id not found')
+    throw new Error('id not found')
   }
 
   return codelogs[codelogIndex]
 }
 
-/**
- * Add a new codelog to the array of codelogs and store the array in the localStorage
- * @param codelog - A new codelog to be added to the array of codelogs
- */
-export const addNewCodelog = (codelog: Codelog): void => {
+export const addNewCodelog = (codelog: Codelog): Codelog => {
   const codelogs = getAllCodelogs()
+  codelog.id = generateId()
   saveCodelog([...codelogs, codelog])
+  return codelog
 }
 
-/**
- * Edit a codelog
- * @param id - The id of the codelog to be edited
- * @param codelog - a codelog object containing the new data
- * @returns codelog | Error - The edited codelog or Error if the id was not found or the localStorage is empty
- */
-export const editCodelog = (id: number, codelog: Codelog): Error | Codelog => {
+export const editCodelog = (codelog: Codelog): Codelog => {
   const codelogs = getAllCodelogs()
 
-  if (!codelogs) return Error('Database is empty')
+  if (!codelogs) throw new Error('Database is empty')
 
-  const codelogIndex = codelogs.findIndex((codelog) => codelog.id === id)
-  if (codelogIndex === -1) return Error('Id not found')
+  const codelogIndex = codelogs.findIndex((clog) => clog.id === codelog.id)
+  if (codelogIndex === -1) throw new Error('id not found')
 
-  const { title, date, tasks, blockers, todayILearned } = codelog
-  const updatedCodelog: Codelog = { id, date, title, tasks, blockers, todayILearned }
-  codelogs[codelogIndex] = updatedCodelog
+  codelogs[codelogIndex] = codelog
   saveCodelog([...codelogs])
-  return updatedCodelog
+  return codelogs[codelogIndex]
 }
 
-/**
- * Delete a codelog
- * @param id Codelog's id
- * @returns Codelog | Error - The deleted Codelog or Error if the id was not found or if the localStorage is empty
- */
-export const deleteCodelog = (id: number): Error | Codelog => {
+export const deleteCodelog = (id: number) => {
   const codelogs = getAllCodelogs()
 
-  if (!codelogs) return Error('Database is empty')
+  if (codelogs.length === 0) throw new Error('Database is empty')
 
   const codelogIndex = codelogs.findIndex((codelog) => codelog.id === id)
-  if (codelogIndex === -1) return Error('id not found')
+  if (codelogIndex === -1) throw new Error('id not found')
 
   const deletedCodelog = codelogs.splice(codelogIndex, 1)
   saveCodelog([...codelogs])
-  return deletedCodelog[0]
+  return [...codelogs]
 }
--- src/tests/api.test.tsx
+++ src/tests/api.test.tsx
@@ -1,5 +1,5 @@
+import { Codelog } from '../../src/store/codelog/types'
 import * as api from '../api'
-import Codelog from '../types/codelog'
 
 test('when the app runs for the first time, api returns an empty array', () => {
   const codelogs = api.getAllCodelogs()
@@ -9,91 +9,83 @@ test('when the app runs for the first time, api returns an empty array', () => {
 describe('CRUD', () => {
   const codelogs: Codelog[] = [
     {
-      id: 1,
+      id: 0,
       date: new Date(),
       title: 'here comes the title',
       tasks: 'Write the API, document it, write tests',
       blockers: "Don't know how to document TypeScript project"
     },
     {
-      id: 2,
+      id: 1,
       date: new Date(),
       title: 'Other title',
       tasks: 'Write more tests...'
     }
   ]
 
+  test('get codelog by id should throw an Error when the localStorage is empty', () => {
+    // localStorage.clear()
+    // const log = api.getCodelogById(11)
+    expect(() => api.getCodelogById(111)).toThrow('id not found')
+  })
+
+  beforeAll(() => {
+    localStorage.clear()
+    api.addNewCodelog(codelogs[1])
+  })
+
   test('add new codelog', () => {
     api.addNewCodelog(codelogs[0])
     const logs = api.getAllCodelogs()
-    expect(logs.length).toBe(1)
-  })
-
-  test('the only item in the localStorage shoudl be equal to codelogs[0]', () => {
-    const logs = api.getAllCodelogs()
-    expect(JSON.stringify(logs[0])).toBe(JSON.stringify(codelogs[0]))
+    expect(logs.length).toBe(2)
   })
 
   test('get codelog by id', () => {
-    const log = api.getCodelogById(1)
-    expect(JSON.stringify(log)).toBe(JSON.stringify(codelogs[0]))
+    const allCodelogs = api.getAllCodelogs()
+    expect(allCodelogs.length).toBe(2)
+
+    const log = api.getCodelogById(allCodelogs[0].id)
+    expect(JSON.stringify(log)).toBe(JSON.stringify(codelogs[1]))
   })
 
-  test('get codelog by id should return error in case of non-existent id', () => {
-    const log = api.getCodelogById(11)
-    expect(log).toBeInstanceOf(Error)
+  test('get codelog by id throw return error in case of non-existent id', () => {
+    expect(() => api.getCodelogById(111)).toThrow('id not found')
   })
 
   test('edit codelog should succeed', () => {
-    const codelog: Codelog = {
-      id: 1,
-      date: codelogs[0].date,
-      title: 'Yet other title',
-      tasks: 'keep rocking the tests',
-      blockers: ''
-    }
-    const editedCodelog = api.editCodelog(1, codelog)
+    const codelog: Codelog = api.getAllCodelogs()[0]
+    codelog.title = 'This is new title'
+    codelog.blockers = 'Tests sucks (sometimes)'
+    codelog.todayILearned = 'Tests helps to fix functions after changing it'
+    const editedCodelog = api.editCodelog(codelog)
     expect(JSON.stringify(codelog)).toBe(JSON.stringify(editedCodelog))
   })
 
-  test('edit codelog with wrong id whould return an Error', () => {
+  test('edit codelog with wrong id shold throw an Error', () => {
     const codelog: Codelog = {
-      id: 1,
+      id: -1111,
       date: codelogs[0].date,
       title: 'Yet other title',
       tasks: 'keep rocking the tests',
       blockers: ''
     }
-    const editedCodelog = api.editCodelog(11, codelog)
-    expect(editedCodelog).toBeInstanceOf(Error)
-  })
-
-  test('get codelog by id should return an Error when the localStorage is empty', () => {
-    localStorage.clear()
-    const log = api.getCodelogById(11)
-    expect(log).toBeInstanceOf(Error)
+    expect(() => api.editCodelog(codelog)).toThrow('id not found')
   })
 
   test('delete codelog', () => {
-    api.addNewCodelog(codelogs[0])
-    api.addNewCodelog(codelogs[1])
-    const deletedCodelog = api.deleteCodelog(1)
-    expect(JSON.stringify(deletedCodelog)).toBe(JSON.stringify(codelogs[0]))
+    const allCodelogs = api.getAllCodelogs()
+    expect(allCodelogs.length).toBe(2)
+    api.deleteCodelog(allCodelogs[0].id)
+    expect(api.getAllCodelogs().length).toBe(1)
   })
 
-  test('after deleting one item, should have one item', () => {
-    const logs = api.getAllCodelogs()
-    expect(logs.length).toBe(1)
+  test('delete codelog with wrong id should throw an Error', () => {
+    expect(() => api.deleteCodelog(111)).toThrow('id not found')
   })
 
-  test('delete codelog with wrong id should return an Error', () => {
-    const deletedCodelog = api.deleteCodelog(13)
-    expect(deletedCodelog).toBeInstanceOf(Error)
-  })
-
-  test('delete codelog with empty storage shoudl return an Error', () => {
+  test('delete codelog with empty storage should throw an Error', () => {
     localStorage.clear()
-    const deletedCodelog = api.deleteCodelog(13)
-    expect(deletedCodelog).toBeInstanceOf(Error)
+    expect(api.getAllCodelogs().length).toBe(0)
+    expect(() => api.deleteCodelog(13)).toThrow('Database is empty')
   })
 })
