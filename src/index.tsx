c4cbdc0aa03c6538d73bbb8b00dee39c1615534a
Fabio Morbec <morbecfm@gmail.com>
2019-09-30 15:15:00 +0100

    Seed db

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,34 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import * as api from './api/index'
 import App from './App'
 import * as serviceWorker from './serviceWorker'
+import { Codelog } from './store/codelog/types'
+
+let tests: Codelog[] = [
+  {
+    id: 0,
+    date: new Date(),
+    title: 'just a test',
+    tasks: 'dha dha dha ...'
+  },
+  {
+    id: 1,
+    date: new Date(),
+    title: 'just another test',
+    tasks: 'dha dha dha ...'
+  },
+  {
+    id: 1,
+    date: new Date(),
+    title: 'last test',
+    tasks: 'dha dha dha ...',
+    blockers: 'React Hooks :('
+  }
+]
+
+api.deleteAll()
+tests.forEach((log) => api.addNewCodelog(log))
 
 ReactDOM.render(<App />, document.getElementById('root'))
 
