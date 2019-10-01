dbeaf312e916ef8bf876ea641b8708354dbf744d
Fabio Morbec <morbecfm@gmail.com>
2019-10-01 16:15:46 +0100

    Refactor Navbar using Material-UI

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,18 +1,44 @@
-import React, { CSSProperties } from 'react'
+import { AppBar, IconButton, Toolbar, Typography } from '@material-ui/core/'
+import { createStyles, makeStyles, styled, Theme } from '@material-ui/core/styles'
+import Add from '@material-ui/icons/Add'
+import React from 'react'
 
-const Navbar = () => {
-  const style: CSSProperties = {
-    marginLeft: '30px'
-  }
+// const useStyles = makeStyles((theme: Theme) =>
+const useStyles = makeStyles((theme: Theme) =>
+  createStyles({
+    root: {
+      flexGrow: 1
+    },
+    menuButton: {
+      marginLeft: theme.spacing(2)
+    },
+    title: {
+      flexGrow: 1,
+      textAlign: 'center'
+    }
+  })
+)
+
+const Navbar = styled(AppBar)({
+  backgroundColor: '#FE6B8B'
+})
+
+export default function ButtonAppBar() {
+  const classes = useStyles()
+
   return (
-    <nav>
-      <div className="nav-wrapper">
-        <span style={style} className="brand-logo center">
-          Codelogs
-        </span>
-      </div>
-    </nav>
+    <div className={classes.root}>
+      <Navbar position="static">
+        <Toolbar>
+          <Typography variant="h6" className={classes.title}>
+            Codelogs
+          </Typography>
+          <IconButton edge="end" className={classes.menuButton} color="inherit" aria-label="add">
+            <Add />
+          </IconButton>
+          {/* <Button color="inherit">Add</Button> */}
+        </Toolbar>
+      </Navbar>
+    </div>
   )
 }
-
-export default Navbar
