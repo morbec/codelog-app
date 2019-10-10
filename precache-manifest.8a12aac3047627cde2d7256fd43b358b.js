7cc88c8b940a463c50bbb0d0510cbc97878da8eb
Fabio Morbec <morbecfm@gmail.com>
2019-10-10 22:20:40 +0100

    Updates

---
+++ precache-manifest.8a12aac3047627cde2d7256fd43b358b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f03a339101897f1678149cf0d8e6e14c",
+    "url": "/codelog-app/index.html"
+  },
+  {
+    "revision": "ea23175d9d5cf002eeb3",
+    "url": "/codelog-app/static/css/main.45e96922.chunk.css"
+  },
+  {
+    "revision": "52d868c67fa38b6b0f6b",
+    "url": "/codelog-app/static/js/2.1be7f3f3.chunk.js"
+  },
+  {
+    "revision": "ea23175d9d5cf002eeb3",
+    "url": "/codelog-app/static/js/main.e76e5d4c.chunk.js"
+  },
+  {
+    "revision": "38ba770dce6f81661206",
+    "url": "/codelog-app/static/js/runtime-main.124468b3.js"
+  }
+]);
